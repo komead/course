2e28778c04c29fac050c30596572b8601751d360
Komead <dkapitanov18@gmail.com>
2025-01-16 16:56:25 +0300

    Вторая часть урока 2

--- src/lesson1_2/Level1_2.java
+++ src/lesson1_2/Level1_2.java
@@ -2,27 +2,60 @@ package lesson1_2;
 
 public class Level1_2 {
     public static void main(String[] args) {
+        //Задачи не из методички:
+        System.out.println("\nЗадание 1:");
         int[] arr1 = {1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0};
         task1(arr1);
         printArr(arr1);
 
+        System.out.println("\nЗадание 2:");
         int[] arr2 = new int[8];
         task2(arr2);
         printArr(arr2);
 
+        System.out.println("\nЗадание 3:");
         int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         task3(arr3);
         printArr(arr3);
 
-        System.out.println();
+        System.out.println("\nЗадание 4:");
         int[][] arr4 = new int[5][5];
         task4(arr4);
 
+        System.out.println("\nЗадание 5:");
         int[] arr5 = {10, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         task5(arr5);
 
+        System.out.println("\nЗадание 6:");
         int[] arr6 = {1, 1, 2, 2, 3, 4, 5};
-        System.out.println("\n" + task6(arr6));
+        System.out.println(task6(arr6));
+
+        System.out.println("\nЗадание 7:");
+        int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int n = 3;
+        task7(arr7, n);
+
+        //Задачи из методички:
+        System.out.println("\nЗадание 8:");
+        int a = 13, b = 3;
+        System.out.println(task8(a, b));
+
+        System.out.println("\nЗадание 9:");
+        a = -2;
+        task9(a);
+
+        System.out.println("\nЗадание 10:");
+        a = -2;
+        System.out.println(task10(a));
+
+        System.out.println("\nЗадание 10:");
+        n = 3;
+        String str = "Hello world";
+        task11(str, n);
+
+        System.out.println("\nЗадание 10:");
+        int year = 2025;
+        task12(year);
     }
 
     //Задания не из методички
@@ -50,9 +83,10 @@ public class Level1_2 {
             arr[i][i] = 1;
 
         for (int[] i : arr) {
-            System.out.println();
             for (int j : i)
                 System.out.print(j + " ");
+
+            System.out.println();
         }
     }
 
@@ -63,7 +97,7 @@ public class Level1_2 {
             min = Math.min(min, number);
             max = Math.max(max, number);
         }
-        System.out.println("\n\nmin: " + min + " max: " + max);
+        System.out.println("min: " + min + " max: " + max);
     }
 
     public static String task6(int[] arr) {
@@ -80,10 +114,46 @@ public class Level1_2 {
         return "false";
     }
 
+    private static void task7(int[] arr, int shift) {
+        int temp = 0, i = 0, counter = 0;
+        while (counter < arr.length) {
+
+            //ещё не сделал
+            counter++;
+        }
+    }
+
+    //Задачи из методички
+    public static boolean task8(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber >= 10 && firstNumber + secondNumber <= 20;
+    }
+
+    private static void task9(int number) {
+        if (number < 0)
+            System.out.println("Отрицательное");
+        else
+            System.out.println("Положительное");
+    }
+
+    private static boolean task10(int number) {
+        return number < 0;
+    }
+
+    private static void task11(String str, int n) {
+        for (int i = 0; i < n; i++)
+            System.out.println(str);
+    }
+
+    private static void task12(int year) {
+        //ещё не сделал
+    }
+
     public static void printArr(int[] arr) {
-        System.out.print("\n\nРезультат: ");
+        System.out.print("Результат: ");
 
         for (int number : arr)
             System.out.print(number + " ");
+
+        System.out.println();
     }
 }
