43a3af266b216f9b837d5ce536faba88abd48aae
komead <kapitanDG@mail.ru>
2025-01-18 18:33:35 +0300

    Завершил задачи по уроку 2

--- src/lesson1_2/Level1_2.java
+++ src/lesson1_2/Level1_2.java
@@ -32,8 +32,9 @@ public class Level1_2 {
 
         System.out.println("\nЗадание 7:");
         int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int n = 3;
+        int n = -3;
         task7(arr7, n);
+        printArr(arr7);
 
         //Задачи из методички:
         System.out.println("\nЗадание 8:");
@@ -45,17 +46,17 @@ public class Level1_2 {
         task9(a);
 
         System.out.println("\nЗадание 10:");
-        a = -2;
+        a = -1;
         System.out.println(task10(a));
 
-        System.out.println("\nЗадание 10:");
+        System.out.println("\nЗадание 11:");
         n = 3;
         String str = "Hello world";
         task11(str, n);
 
-        System.out.println("\nЗадание 10:");
-        int year = 2025;
-        task12(year);
+        System.out.println("\nЗадание 12:");
+        int year = 2100;
+        System.out.println(task12(year));
     }
 
     //Задания не из методички
@@ -115,11 +116,25 @@ public class Level1_2 {
     }
 
     private static void task7(int[] arr, int shift) {
-        int temp = 0, i = 0, counter = 0;
-        while (counter < arr.length) {
+        int k = shift < 0 ? -1 : 1;                 //коэффициент, который определяет в какую сторону сдвигается массив
+        int previousNumber = arr[arr.length - 1];   //число, которое поменяли в предыдущей итерации
+        int currentNumber;
+        int currentIndex = 0, previousIndex;        //индексы для чисел выше
 
-            //ещё не сделал
-            counter++;
+        //данный цикл сдвигает массив на 1 элемент |shift| раз
+        for (int i = 0; i < Math.abs(arr.length * shift); i++) {
+            //для первого элемента массива обновляется previousNumber
+            if (currentIndex == 0) {
+                previousIndex = (arr.length - k) % arr.length;
+                previousNumber = arr[previousIndex];
+            }
+            //сохраняется текущее число, на его место становится предыдущее, текущее становится предыдущим и меняется индекс
+            currentNumber = arr[currentIndex];
+            arr[currentIndex] = previousNumber;
+
+            previousNumber = currentNumber;
+
+            currentIndex = (arr.length + currentIndex + k) % arr.length;
         }
     }
 
@@ -144,8 +159,8 @@ public class Level1_2 {
             System.out.println(str);
     }
 
-    private static void task12(int year) {
-        //ещё не сделал
+    private static boolean task12(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
     public static void printArr(int[] arr) {
