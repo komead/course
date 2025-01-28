8fb7a395c7c3c4e05c7d6e187b24990f244c0788
Komead <dkapitanov18@gmail.com>
2025-01-28 16:46:46 +0300

    Добавил комментарии и улучшил код

--- src/lesson1_4/TicTacToe.java
+++ src/lesson1_4/TicTacToe.java
@@ -11,27 +11,16 @@ public class TicTacToe {
 
     private char[][] field;
 
-    private final Random random = new Random();
     private final Scanner scanner = new Scanner(System.in);
 
     public void play() {
-        System.out.println("Введите размер квадратного игрового поля: ");
-        setFieldParams(scanner.nextInt());
-
-        if (fieldSize < 3) {
-            setFieldParams(3);
-            System.out.println("Размер поля не может быть таким маленьким. Установлен стандартный размер поля.");
-        }
-        field = new char[fieldSize][fieldSize];
         int stepCounter = 0;
-        clearBoard();
 
-        if (random.nextBoolean()) {
-            setFigures('x', 'o');
-        } else {
-            setFigures('o', 'x');
-        }
-        System.out.println("Вы играете за " + playerFigure);
+        setFieldParams();
+        field = new char[fieldSize][fieldSize];
+
+        clearBoard();
+        setFigures();
 
         char winner;
         while (stepCounter < fieldSize * fieldSize) {
@@ -56,9 +45,14 @@ public class TicTacToe {
 
             int yPos, xPos;
             while (true) {
-                System.out.println("Введите координаты выбранной ячейки: ");
+                System.out.println("Введите координаты выбранной ячейки через пробел  (Номер_строки Номер_столбца): ");
                 yPos = scanner.nextInt();
                 xPos = scanner.nextInt();
+
+                if (yPos >= fieldSize || xPos >= fieldSize) {
+                    System.out.println("Нет такой координаты");
+                    continue;
+                }
 
                 if (cellIsAvailable(yPos, xPos)) {
                     break;
@@ -66,7 +60,6 @@ public class TicTacToe {
                 System.out.println("Данная ячейка занята!");
             }
             playerMove(yPos, xPos);
-//            printField(field);
             winner = checkWinner();
             stepCounter++;
 
@@ -85,38 +78,47 @@ public class TicTacToe {
         // Сюда будут записываться кол-во фигур игрока, кол-во фигур компьютера, индексы первого элемента и направления линий
         int[] bestCombination = new int[] {0, 0, 0, 0, 0, 0};
 
+        // Проходимся по полю
         for (int i = 0; i < fieldSize; i++) {
             for (int j = 0; j < fieldSize; j++) {
+                // Проверяем главную диагональ
                 if (i < fieldSize - lineLength + 1 && j < fieldSize - lineLength + 1) {
                     bestCombination = checkCombination(bestCombination, i, j, 1, 1);
 
+                    // Если осталось поставить только 1 фигуру, то ставим её
                     if (bestCombination[0] == 0 && (lineLength - bestCombination[0] - bestCombination[1] == 1)) {
                         setComputerFigureOnBoard(bestCombination);
                         return;
                     }
                 }
 
+                // Проверяем побочную диагональ
                 if (i > lineLength - 2 && j < fieldSize - lineLength + 1) {
                     bestCombination = checkCombination(bestCombination, i, j, -1, 1);
 
+                    // Если осталось поставить только 1 фигуру, то ставим её
                     if (bestCombination[0] == 0 && (lineLength - bestCombination[0] - bestCombination[1] == 1)) {
                         setComputerFigureOnBoard(bestCombination);
                         return;
                     }
                 }
 
+                // Проверяем горизонтальную линию
                 if (j < fieldSize - lineLength + 1) {
                     bestCombination = checkCombination(bestCombination, i, j, 0, 1);
 
+                    // Если осталось поставить только 1 фигуру, то ставим её
                     if (bestCombination[0] == 0 && (lineLength - bestCombination[0] - bestCombination[1] == 1)) {
                         setComputerFigureOnBoard(bestCombination);
                         return;
                     }
                 }
 
+                // Проверяем вертикальную линию
                 if (i < fieldSize - lineLength + 1) {
                     bestCombination = checkCombination(bestCombination, i, j, 1, 0);
 
+                    // Если осталось поставить только 1 фигуру, то ставим её
                     if (bestCombination[0] == 0 && (lineLength - bestCombination[0] - bestCombination[1] == 1)) {
                         setComputerFigureOnBoard(bestCombination);
                         return;
@@ -128,8 +130,10 @@ public class TicTacToe {
         setComputerFigureOnBoard(bestCombination);
     }
 
+    // Метод устанавливает фигуру компьютера в первую пустую линию для полученной комбинации
     public void setComputerFigureOnBoard(int[] bestCombination) {
         for (int k = 0; k < lineLength; k++) {
+            // Вычисляем индекс текущей ячейки
             int i = bestCombination[2] + k * bestCombination[4];
             int j = bestCombination[3] + k * bestCombination[5];
 
@@ -140,11 +144,13 @@ public class TicTacToe {
         }
     }
 
+    // Метод вычисляет лучшую комбинацию для хода компьютера
     public int[] checkCombination(int[] best, int i, int j, int iShift, int jShift) {
         int playerFigureCounter = 0;
         int computerFigureCounter = 0;
         int spaceCounter = 0;
 
+        // Проходимся по линии и считаем сколько каких фигур в неё есть
         for (int k = 0; k < fieldSize; k++) {
             if (field[i][j] == computerFigure)
                 computerFigureCounter++;
@@ -157,6 +163,7 @@ public class TicTacToe {
             j += jShift;
         }
 
+        // Если линия полностью заполнена, то дальше нет смысла проверять
         if (spaceCounter == 0)
             return best;
 
@@ -165,17 +172,17 @@ public class TicTacToe {
 
         if (playerFigureCounter > best[0] || (playerFigureCounter == 0 && spaceCounter == 1)) {
             return new int[] {playerFigureCounter, computerFigureCounter, i, j, iShift, jShift};
-        } /*else if (playerFigureCounter == best[0]) {
+        }
 
-        }*/
-//        return null;
         return best;
     }
 
+    // Метод устанавливает фигуру игрока в указанную ячейку
     public void playerMove(int yPos, int xPos) {
         field[yPos][xPos] = playerFigure;
     }
 
+    // Метод проверяет есть ли линия с одинаковыми фигурами. Возвращает фигуру победителя или пробел
     public char checkWinner() {
         for (int i = 0; i < fieldSize; i++) {
             for (int j = 0; j < fieldSize; j++) {
@@ -192,10 +199,12 @@ public class TicTacToe {
         return ' ';
     }
 
+    // Метод проверяет, является ли выбранная ячейка пустой
     public boolean cellIsAvailable(int yPos, int xPos) {
         return field[yPos][xPos] == ' ';
     }
 
+    // Метод очищает доску
     public void clearBoard() {
         for (int i = 0; i < fieldSize; i++) {
             for (int j = 0; j < fieldSize; j++) {
@@ -204,21 +213,49 @@ public class TicTacToe {
         }
     }
 
-    public void setFieldParams(int fieldSize) {
-        this.fieldSize = fieldSize;
-
-        if (fieldSize > 4) {
-            this.lineLength = 4;
-        } else {
-            this.lineLength = 3;
+    // Метод устанавливает размер поля и длину линии, которую нужно собрать для победы
+    public void setFieldParams() {
+        while (true) {
+            System.out.println("""
+                    Выберите размер игрового поля:
+                    1 - 3x3
+                    2 - 4x4
+                    3 - 5x5""");
+            switch (scanner.nextInt()) {
+                case 1:
+                    this.fieldSize = 3;
+                    lineLength= 3;
+                    return;
+                case 2:
+                    this.fieldSize = 4;
+                    lineLength = 3;
+                    return;
+                case 3:
+                    this.fieldSize = 5;
+                    lineLength = 4;
+                    return;
+                default:
+                    System.out.print("Нет такого варианта!" );
+                    break;
+            }
         }
     }
 
-    public void setFigures(char playerFigure, char computerFigure) {
-        this.playerFigure = playerFigure;
-        this.computerFigure = computerFigure;
+    // Метод устанавливает фигурки для игроков
+    public void setFigures() {
+        Random random = new Random();
+
+        if (random.nextBoolean()) {
+            this.playerFigure = 'x';
+            this.computerFigure = 'o';
+        } else {
+            this.playerFigure = 'o';
+            this.computerFigure = 'x';
+        }
+        System.out.println("Вы играете за " + playerFigure);
     }
 
+    // Метод выводит поле
     public void printField(char[][] arr) {
         System.out.print(" ");
         for (int i = 0; i < arr.length; i++) {
@@ -231,11 +268,6 @@ public class TicTacToe {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(" " + arr[i][j] + " |");
             }
-//            System.out.print("\n   ");
-//
-//            for (int j = 0; j < arr[i].length * 4 - 1; j++) {
-//                System.out.print("—");
-//            }
             System.out.println();
         }
         System.out.println();
