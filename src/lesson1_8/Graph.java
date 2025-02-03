aca787718cc6e0824ba6df93c896f5b8eadc4d63
Komead <dkapitanov18@gmail.com>
2025-02-03 16:49:38 +0300

    commit

---
+++ src/lesson1_8/Graph.java
@@ -0,0 +1,15 @@
+package lesson1_8;
+
+import javax.swing.*;
+
+public class Graph extends JFrame {
+    public Graph(String expression) {
+        setTitle("Main Window");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setBounds(700, 300, 400, 400);
+        setResizable(false);
+
+
+        setVisible(true);
+    }
+}
--- src/lesson1_8/StartWindow.java
+++ src/lesson1_8/StartWindow.java
@@ -1,6 +1,7 @@
 package lesson1_8;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StartWindow extends JFrame {
     JButton calculate;
@@ -9,15 +10,30 @@ public class StartWindow extends JFrame {
     public StartWindow() {
         setTitle("Main Window");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(300, 300, 400, 200);
+        setBounds(300, 300, 400, 100);
+        setResizable(false);
 
         calculate = new JButton("Calculate");
-        expression = new JTextField(10);
+        expression = new JTextField();
+
+        setListener();
+
+        expression.setMaximumSize(new Dimension(Integer.MAX_VALUE, expression.getPreferredSize().height));
 
         setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+        add(Box.createHorizontalStrut(10)); // Отступ между элементами
         add(expression);
+        add(Box.createHorizontalStrut(10)); // Отступ между элементами
         add(calculate);
+        add(Box.createHorizontalStrut(10)); // Отступ между элементами
+
 
         setVisible(true);
+    }
+
+    private void setListener() {
+        calculate.addActionListener(e -> {
+            new Graph("");
+        });
     }
 }
